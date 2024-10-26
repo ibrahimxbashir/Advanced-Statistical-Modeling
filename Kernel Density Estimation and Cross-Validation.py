cb20dfd07c04e276f51b8a291cc2437eec713f3d
Ibrahim Bashir <45203276+ibrahimxbashir@users.noreply.github.com>
2024-10-26 20:31:06 +0300

    Update Kernel Density Estimation and Cross-Validation.py

--- Kernel Density Estimation and Cross-Validation.py
+++ Kernel Density Estimation and Cross-Validation.py
@@ -45,38 +45,3 @@ def CV_criterion(h, X, d):
     return p2 - 2*Tn
     pass
 
-
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-
-if __name__ == '__main__':
-
-    # TODO: YOUR CODE HERE
-    X1, X2 = norm.rvs(size=100), norm.rvs(size=1000)  # Generate samples for n=100 and n=1000
-    h = np.linspace(0.01, 3, 50)  # Generate 50 evenly spaced points for h to optimize through
-    x = np.linspace(min(min(X1), min(X2)), max(max(X1), max(X2)),
-                    100)  # Generate 100 evenly spaced points along x-axis within ranges of X1,X2
-    pdf = norm.pdf(x, 0, 1)  # The pdf of N(0,1) along points in x
-
-
-    def plotter(axes, X, d, cols):
-        for i in range(len(cols)):  # Calculate KDE at each point xi given sample X, order d, and plot
-            Xi, di = X[i // 2], d[i % 2]  # Define variable for sample and order of iteration
-            min_index, min_value = min(((i, CV_criterion(hi, Xi, di)) for i, hi in enumerate(h)), key=lambda x: x[
-                1])  # Get the index of the min value of CV(h) where hi in H, and then index H[i] to get optimal bandwidths
-            kerns = np.array(
-                [KDE(xi, Xi, h[min_index], di) for xi in x])  # Get KDE of Xi at x, of order di with optimal bandwidth
-            axes.plot(x, kerns, lw=2, label=f'X{i // 2 + 1}, K{di}',
-                      color=cols[i])  # Plot KDE with appropriate label and color
-
-
-    fig, ax = plt.subplots(figsize=(10, 7))  # Plot data
-    ax.hist(X1, bins='auto', density=True, label='X1, n=100', color='yellow', alpha=0.3)  # Histogram of samples
-    ax.hist(X2, bins='auto', density=True, label='X2, n=1000', color='orchid', alpha=0.3)
-    ax.plot(x, pdf, lw=2, label='true pdf', color='blue')  # True pdf of N(0,1)
-    plotter(ax, [X1, X2], [1, 2],
-            ['gold', 'goldenrod', 'plum', 'mediumorchid'])  # Plot each of the KDE's with their optimal bandwidth
-    plt.title("KDE of N(0,1) using Legendre polynomials of order {1,2}")
-    plt.legend()
-    plt.show()
-    pass
